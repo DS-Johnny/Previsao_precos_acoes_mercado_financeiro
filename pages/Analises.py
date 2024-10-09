3db11dc5e521f27f7bf281a0be8cda21bedf3862
Johnny Matos <128853698+DS-Johnny@users.noreply.github.com>
2024-10-09 15:00:51 -0300

    Plotly funcionando, testar deploy

--- pages/Analises.py
+++ pages/Analises.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import utils as u
+import plotly_express as px
+import pandas as pd
 
 st.sidebar.title('Análises')
 st.title("Análise")
@@ -29,6 +31,19 @@ st.write(tic, per, inter)
 
 fig = u.plot_graph(tic, per, inter)
 st.plotly_chart(fig, use_container_width=True)
+
+# web scraping 
+ws = {'Anos' : [2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024],
+      'ROE' : [0.65, -33.81, 10.46, 12.26, 15.06, -4.37, 15.16, 63.01, 51.27, 20.91, 23.72],
+      'ROIC' : [7.7, -19.7, 5.01, 13.72, 18.33, -0.13, 19.31, 43.08, 29.65, 19.46, 22.11],
+      'ROA' : [0.07, -13.31, 4.12, 5.39, 7.53, -2.35, 5.21, 24.33, 21.25, 8.89, 10.17]
+      }
+
+ws = pd.DataFrame(ws)
+
+fig2 = px.line(ws, 'Anos', y=['ROE', 'ROIC', 'ROA'])
+st.plotly_chart(fig2, use_container_width=True)
+
 
 
 tab1, tab2 = st.tabs(['Gráficos', 'Tabelas'])
